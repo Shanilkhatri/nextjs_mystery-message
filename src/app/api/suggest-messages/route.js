f16f99ce7c1da1e43bf7215cb240729d6ae344cb
shanilkhatri <shanilkhatri1@gmail.com>
2024-08-03 23:05:46 +0530

    added openAI from vercel but still doubt it's gonna coz the API isn't paid

---
+++ src/app/api/suggest-messages/route.js
@@ -0,0 +1,35 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+
+
+const openai = new OpenAI({ 
+    apiKey: process.env.OPENAI_API_KEY 
+});
+
+export const runtime = 'edge';
+
+export async function POST(req) {
+  try{
+      const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?|| What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
+    
+      const result = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        max_tokens: 400,
+        stream: true,
+        prompt,
+      });
+      
+      const stream = OpenAIStream(result);
+
+      return new StreamingTextResponse(stream);
+  }catch(error){
+    if(error instanceof OpenAI.APIError){
+        const{name, status,headers, message} = error;
+        console.log("OpenAI API Error: ",{name, status, headers, message});
+        return Response.json({name, status,headers, message}, {status: 500});
+    }else{
+        console.error("An unknown error occurred",error);
+        throw error;
+    }
+}
+}
